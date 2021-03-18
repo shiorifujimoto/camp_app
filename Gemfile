f03fd338683bcc2d75c23c7040c543e24742670a
fujifuji123412 <sena.04091919@gmail.com>
2021-03-18 13:46:54 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -61,3 +61,6 @@ gem 'omniauth-facebook'
 gem 'omniauth-google-oauth2'
 gem "omniauth-rails_csrf_protection"
 gem 'omniauth', '~>1.9.1'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   has_many :sns_credentials
-
+  has_many :posts
+  
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
     user = User.where(email: auth.info.email).first_or_initialize(
