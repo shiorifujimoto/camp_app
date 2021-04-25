1da58875cfc5d20f6530f5ee6f325dca151fe052
fujifuji123412 <sena.04091919@gmail.com>
2021-04-25 18:01:11 +0900

    ルーテング（create）

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: 'users#index' 
   resources :users, only: [:index, :new, :show, :edit, :update]  
   resources :posts do
+    resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
   end
